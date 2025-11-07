a69ff078dc9f4af4626e5f4f27f8518aa6f0038b
simonemastria <simone.mastria@studio.unibo.it>
2025-11-07 10:19:18 +0100

    test update package.json and fix testRunner tests

--- src/testRunner.test.js
+++ src/testRunner.test.js
@@ -3,9 +3,11 @@
  * Ensures Create React App runs tests placed outside /src.
  */
 import "../tests/conflictUtils.test.js";      // già presente nel repo
+import "../tests/App.test.js";                // già presente nel repo
+
+// Nuovi test aggiunti
 import "../tests/conflictUtils.extra.test.js";
 import "../tests/eventUtils.test.js";
 import "../tests/ScheduleContainer.render.test.jsx";
 import "../tests/ScheduleContainer.conflict-smoke.test.jsx";
 import "../tests/api.error.test.js";
-import "../tests/api.fetch-success.test.js";
